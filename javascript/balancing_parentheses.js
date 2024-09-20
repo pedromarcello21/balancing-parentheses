01d01e85a0263298c2e9ac55a97f28ce8e597d1c
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-09-20 09:54:18 -0400

    solution

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,6 +1,39 @@
 function balancingParentheses(string) {
   // type your code here
+  let openCount = 0;
+  let closeCount = 0;
+  values = string.split("");
+  for(i in values){
+    if(values[i] == "("){
+      openCount ++;
+    } else if (values[i] == ")"){
+      if (openCount > 0){
+        openCount --
+      } else{
+        closeCount ++
+      }
+    }
+  }
+  return openCount + closeCount
 }
+
+////I lowkey like my way better but doesn't pass test of accounting for strings that start w closing or end w opening paren
+
+
+// function balancingParentheses(string) {
+//   // type your code here
+//   let balance = 0;
+//   values = string.split("");
+//   for(i in values){
+//     if(values[i] == "("){
+//       balance ++;
+//     } else{
+//       balance --;
+//     }
+//   }
+//   return Math.abs(balance)
+// }
+
 
 if (require.main === module) {
   // add your own tests in here
